9208cb52df819e7cee355dad308b97e1c57f7ac9
chat-wane <grumpy.chat.wane@gmail.com>
2022-12-20 17:58:03 +0100

    exploring jena

--- src/main/java/fr/gdd/App.java
+++ src/main/java/fr/gdd/App.java
@@ -4,6 +4,12 @@ import java.util.Iterator;
 import java.util.stream.Stream;
 
 import org.apache.jena.atlas.lib.tuple.Tuple;
+import org.apache.jena.atlas.lib.tuple.TupleFactory;
+import org.apache.jena.dboe.trans.bplustree.BPTreeNode;
+import org.apache.jena.dboe.trans.bplustree.BPTreePage;
+import org.apache.jena.dboe.trans.bplustree.BPlusTree;
+import org.apache.jena.graph.Node;
+import org.apache.jena.graph.NodeFactory;
 import org.apache.jena.query.Dataset;
 import org.apache.jena.sparql.core.DatasetGraph;
 import org.apache.jena.sparql.core.Quad;
@@ -12,23 +18,80 @@ import org.apache.jena.tdb2.DatabaseMgr;
 import org.apache.jena.tdb2.store.DatasetGraphTDB;
 import org.apache.jena.tdb2.store.NodeId;
 import org.apache.jena.tdb2.store.TripleTable;
+import org.apache.jena.tdb2.store.nodetable.NodeTable;
+import org.apache.jena.tdb2.store.tupletable.TupleIndex;
+import org.apache.jena.tdb2.store.tupletable.TupleTable;
+import org.apache.jena.tdb2.store.nodetupletable.NodeTupleTable;
 import org.apache.jena.tdb2.sys.TDBInternal;
 
 
 
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println( "Opening TDB2 !" );
+        long start = System.currentTimeMillis();
         Dataset ds = TDB2Factory.connectDataset("/Users/nedelec-b-2/Desktop/Projects/preemptable-blazegraph/watdiv10M");
+        long end = System.currentTimeMillis();
 
-        System.out.println("Done connecting to db");
+        System.out.printf("Took %s ms to open TDB.\n", end-start);
         
         DatasetGraph dsg = ds.asDatasetGraph();
         DatasetGraphTDB tdb = TDBInternal.getDatasetGraphTDB(dsg);
+
+        start = System.currentTimeMillis();
         tdb.begin();
 
         TripleTable tt =  tdb.getTripleTable();
-        Iterator<Tuple<NodeId>> iter = tt.getNodeTupleTable().findAll();
+        NodeTupleTable ntt = tt.getNodeTupleTable();
+        NodeTable node_table = ntt.getNodeTable();
+
+        Node uri = NodeFactory.createURI("http://db.uwaterloo.ca/~galuc/wsdbm/City193");
+        NodeId uri_id = node_table.getNodeIdForNode(uri);
+        System.out.printf("%s \n", uri_id);
+        
+        // Tuple<NodeId> pattern = TupleFactory.tuple(uri_id, NodeId.NodeIdAny, NodeId.NodeIdAny);
+
+        Iterator<Tuple<NodeId>> uri_ids = ntt.find(uri_id, NodeId.NodeIdAny, NodeId.NodeIdAny);
+        Tuple<NodeId> found = null;
+        while (uri_ids.hasNext()) {
+            found = uri_ids.next();
+            System.out.printf("%s \n", found);
+        };
+        
+        Node predicate = node_table.getNodeForNodeId(found.get(1));
+        Node object = node_table.getNodeForNodeId(found.get(2));        
+        System.out.printf("%s %s \n", predicate, object);
+        
+        TupleTable tuple_table = ntt.getTupleTable();
+
+        // TODO get the proper index: <https://github.com/apache/jena/blob/c4e999d633b2532b504b35937db4bec9a7c2e539/jena-tdb2/src/main/java/org/apache/jena/tdb2/store/tupletable/TupleTable.java#L133>
+        
+        TupleIndex ti = tuple_table.getIndex(0);
+        // See to create an iterator: <https://github.com/apache/jena/blob/ebc10c4131726e25f6ffd398b9d7a0708aac8066/jena-tdb2/src/main/java/org/apache/jena/tdb2/store/tupletable/TupleIndexRecord.java#L202>
+        // it uses RangeIndex. We can get rangeindex by calling getRangeIndex on TupleIndexRecord that
+        // is a cast of tuple index. 
+        // We can even have BPlusTree from that Range index
+        // see : <https://github.com/apache/jena/blob/ebc10c4131726e25f6ffd398b9d7a0708aac8066/jena-tdb2/src/main/java/org/apache/jena/tdb2/loader/base/LoaderOps.java#L69>
+
+        // Then https://github.com/apache/jena/blob/31dc0d328c4858401e5d3fa99702c97eba0383a0/jena-db/jena-dboe-trans-data/src/main/java/org/apache/jena/dboe/trans/bplustree/BPTreeRangeIterator.java
+
+        // we can get node using access path... <https://github.com/apache/jena/blob/31dc0d328c4858401e5d3fa99702c97eba0383a0/jena-db/jena-dboe-trans-data/src/main/java/org/apache/jena/dboe/trans/bplustree/AccessPath.java>
+
+        
+        // (Not sure useful now but=> especially take a look at recordMapper: <https://github.com/apache/jena/blob/ebc10c4131726e25f6ffd398b9d7a0708aac8066/jena-tdb2/src/main/java/org/apache/jena/tdb2/store/tupletable/TupleIndexRecord.java#L62>
+        
+        
+        // BPTreePage bptp = null;
+        // bptp.getId();
+        // BPTreeNode bptn = null;
+        // bptn.getCount();
+        // bptn.getId();
+        // bptn.search(root, record);
+        // BPlusTree bpt = null;
+        // bpt.find(record);
+        
+        
+        Iterator<Tuple<NodeId>> iter = ntt.findAll();
 
         // iter.forEachRemaining(e -> System.out.println(e));
         long i = 0;
@@ -36,11 +99,10 @@ public class App {
             iter.next();
             i += 1;
         };
+
         
-        // s.forEach((e)-> System.out.println(e.getObject().getIndexingValue()));
-        
-        // long i = s.count();
         tdb.end();
-        System.out.println(String.format("%s elements", i));
+        end = System.currentTimeMillis();
+        System.out.println(String.format("%s elements in %s ms", i, end - start));
     }
 }
