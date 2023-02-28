8fa07f42ea0e7939a5003f3759c91287a4ebd035
chat-wane <grumpy.chat.wane@gmail.com>
2023-02-28 17:40:18 +0100

    removed Pair dependency close #6

--- sage-jena-module/src/main/java/fr/gdd/sage/arq/VolcanoIterator.java
+++ sage-jena-module/src/main/java/fr/gdd/sage/arq/VolcanoIterator.java
@@ -2,12 +2,12 @@ package fr.gdd.sage.arq;
 
 import java.util.Iterator;
 
-import org.apache.jena.atlas.lib.Pair;
 import org.apache.jena.graph.Node;
 import org.apache.jena.sparql.core.Quad;
 import org.apache.jena.tdb2.store.NodeId;
 import org.apache.jena.tdb2.store.nodetable.NodeTable;
 
+import fr.gdd.sage.generics.Pair;
 import fr.gdd.sage.interfaces.BackendIterator;
 import fr.gdd.sage.interfaces.SPOC;
 import fr.gdd.sage.io.SageOutput;
